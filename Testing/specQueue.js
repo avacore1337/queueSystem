64e9b93a8288fc3465d7f8dc97d62ec4043cd0ae
Egeras <Edvard.mickos@gmail.com>
2015-04-29 16:15:04 +0200

    Id fixes for testing

--- Testing/specQueue.js
+++ Testing/specQueue.js
@@ -12,7 +12,8 @@ var ta = 'Admin';
     adminLogIn('delete');
     element(by.id('listdbasBtn')).click();
     closeMOTD();
-    element(by.id('coursePurgeQueueBtn')).click();
+    element(by.id('queueOptionsBtn')).click();
+    element(by.id('queuePurgeQueueBtn')).click();
     acceptDialogue();
   });
 
@@ -54,18 +55,19 @@ function closeMOTD(){
 };
 
 function acceptDialogue(){
-  browser.sleep(100);
-  browser.switchTo().alert().then(
-      function(alert) {  return alert.accept(); },
-      function(err) { }
-      );
+  //browser.sleep(100);
+  //browser.switchTo().alert().then(
+  //    function(alert) {  return alert.accept(); },
+  //   function(err) { }
+  //    );
+  element(by.id('queueDoPurgeBtn')).click();
 };
 
-function userJoinQueue(joinCourse){
-  element(by.id('list'+joinCourse+'Btn')).click();
+function userJoinQueue(joinQueue){
+  element(by.id('list'+joinQueue+'Btn')).click();
   closeMOTD();
-  element(by.id('courseLocationInputField')).sendKeys(location);
-  element(by.id('courseJoinQueueBtn')).click();
+  element(by.id('queueLocationInputField')).sendKeys(location);
+  element(by.id('queueJoinQueueBtn')).click();
 };
 
 function newBrowser(){
@@ -86,13 +88,14 @@ it('he TA class is able to, by interaction, Purge the queue of all Users', funct
   adminLogIn('TA');
   element(by.id('listdbasBtn')).click();
   closeMOTD();
-  element(by.id('coursePurgeQueueBtn')).click();
+  element(by.id('queueOptionsBtn')).click();
+  element(by.id('queuePurgeQueueBtn')).click();
   acceptDialogue();
-  expect(element(by.id('courseEdvardBtn')).isPresent()).toBeFalsy();
+  expect(element(by.id('queueEdvardBtn')).isPresent()).toBeFalsy();
   newBrowser();
   userLogIn(name);
   userJoinQueue('dbas');
-  expect(element(by.id('courseEdvardBtn')).isPresent()).toBeTruthy();
+  expect(element(by.id('queueEdvardBtn')).isPresent()).toBeTruthy();
 });
 
 it('a User should be able to log on', function() {
@@ -101,17 +104,17 @@ it('a User should be able to log on', function() {
     expect(element(by.id('indexNameTextField')).getText()).toEqual('Edvard');
   });
 
-it('a User should be able to choose a course and join a queue.', function() {
+it('a User should be able to choose a queue and join a queue.', function() {
     userLogIn(name);
     userJoinQueue('dbas');
-    expect(element(by.id('courseEdvardBtn')).isPresent()).toBeTruthy();
+    expect(element(by.id('queueEdvardBtn')).isPresent()).toBeTruthy();
   });
 
 it('a User should be able to leave a joined queue.', function() {
     userLogIn(name);
     userJoinQueue('dbas');
-    element(by.id('courseLeaveQueueBtn')).click();
-    expect(element(by.id('courseEdvardBtn')).isPresent()).toBeFalsy();
+    element(by.id('queueLeaveQueueBtn')).click();
+    expect(element(by.id('queueEdvardBtn')).isPresent()).toBeFalsy();
   });
 
 it('Joining a queue with a booked time slot', function(){
@@ -122,16 +125,16 @@ it('The Student class will have the possibility the change their own data in the
     userLogIn(name, student);
     $('#listdbasBtn').click();
     closeMOTD();
-    $('#courseLocationInputField').sendKeys(location);
-    $('#courseCommentInputField').sendKeys('comment1');
-    $('#courseJoinQueueBtn').click();
-    expect(element(by.id('courseEdvardBtn')).getText()).toMatch('1 Edvard Red comment1');
-    $('#courseCommentInputField').clear();
-    $('#courseLocationInputField').clear();
-    $('#courseLocationInputField').sendKeys('Yellow');
-    $('#courseCommentInputField').sendKeys('comment2');
-    $('#courseUpdateInformationBtn').click()
-    expect(element(by.id('courseEdvardBtn')).getText()).toMatch('1 Edvard Yellow comment2');
+    $('#queueLocationInputField').sendKeys(location);
+    $('#queueCommentInputField').sendKeys('comment1');
+    $('#queueJoinQueueBtn').click();
+    expect(element(by.id('queueEdvardBtn')).getText()).toMatch('1 Edvard Red comment1');
+    $('#queueCommentInputField').clear();
+    $('#queueLocationInputField').clear();
+    $('#queueLocationInputField').sendKeys('Yellow');
+    $('#queueCommentInputField').sendKeys('comment2');
+    $('#queueUpdateInformationBtn').click()
+    expect(element(by.id('queueEdvardBtn')).getText()).toMatch('1 Edvard Yellow comment2');
   });
 
 it('Broadcasting in a Queue', function(){
@@ -145,15 +148,16 @@ it('The TA class is able to, by interaction, ‘Kick’ a User from the Queue', 
   adminLogIn('TA');
   $('#listdbasBtn').click();
   closeMOTD();
-  browser.actions().doubleClick($('#course'+name+'Btn')).perform();
-  $('#courseRemoveUser'+name+'Btn').click();
-  expect(element(by.id('course'+name+'Btn')).isPresent()).toBeFalsy();
+  browser.actions().doubleClick($('#queue'+name+'Btn')).perform();
+  $('#queueRemoveUser'+name+'Btn').click();
+  expect(element(by.id('queue'+name+'Btn')).isPresent()).toBeFalsy();
   });
 
 it('TA is able to use the interaction ‘Lock’ or ‘Unlock’ with a queue', function(){  
  adminLogIn('TA');
  $('#listdbasBtn').click();
- $('#courseLockQueueBtn').click();
+ element(by.id('queueOptionsBtn')).click();
+ $('#queueLockQueueBtn').click();
  newBrowser();
  userLogIn(name);
  $('#listdbasBtn').click();
@@ -161,11 +165,12 @@ it('TA is able to use the interaction ‘Lock’ or ‘Unlock’ with a queue', 
  newBrowser();
  adminLogIn('TA');
  $('#listdbasBtn').click();
- $('#courseUnlockQueueBtn').click();
+ element(by.id('queueOptionsBtn')).click();
+ $('#queueUnlockQueueBtn').click();
  newBrowser();
  userLogIn(name);
  $('#listdbasBtn').click();
- expect(browser.getCurrentUrl()).toMatch('http://localhost:8080/#/course/dbas');
+ expect(browser.getCurrentUrl()).toMatch('http://localhost:8080/#/queue/dbas');
   });
 
 it('TA is able to use the interaction ‘new MOTD’ (Message of the Day) with a queue for a session which he is given privileges.', function(){
@@ -179,14 +184,14 @@ it('The users of class Teacher have the system rights to change other users user
 it('The Teacher class is able to Hide or Reveal the Queue. Hiding the Queue will remove the Queue from the list of Queues for Users of the Student class', function(){
  adminLogIn('TA');
  $('#listdbasBtn').click();
- $('#courseHibernateQueueBtn').click();
+ $('#queueHibernateQueueBtn').click();
  newBrowser();
  userLogIn(name);
  expect($('#listdbasBtn').isDisplayed()).toBeFalsy();
  newBrowser();
  adminLogIn('TA');
  $('#listdbasBtn').click();
- $('#courseWakeupQueueBtn').click();
+ $('#queueWakeupQueueBtn').click();
  newBrowser();
  userLogIn(name);
  expect($('#listdbasBtn').isDisplayed()).toBeTruthy();
