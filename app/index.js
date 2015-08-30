968702516ffbd9450da30bd5e66742dcc7935119
Robert Welin-Berger <robertwb@queue.csc.kth.se>
2015-08-30 20:43:29 +0200

    forgot the slash before .. in public path

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ var request = require('request');
 
 mongoose.connect('mongodb://localhost/queueBase');
 
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
